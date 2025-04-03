5dfc9f35cc9b362778b974c813e75f11b8c84983
Chiemi Watanabe <chiemi627@users.noreply.github.com>
2025-04-03 15:03:49 +0000

    IFTTTのパラメタを変更

--- index.js
+++ index.js
@@ -248,7 +248,7 @@ app.post('/sendorders', (req, res) => {
             "Content-type": "application/json",
           },
           json: {
-            "value1": message.join("<br/>")
+            "Value1": message.join("<br/>")
           }
         }
         request.post(options, (error, ifttt_res, body) => {
